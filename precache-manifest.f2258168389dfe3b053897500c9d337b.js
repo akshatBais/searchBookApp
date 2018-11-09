8609b13ecc7fb167588ab76c3f0bd09a3c1842a5
Akshat <akshatsinghbais@gmail.com>
2018-11-09 11:12:55 +0530

    Updates

--- precache-manifest.f2258168389dfe3b053897500c9d337b.js
+++ precache-manifest.f2258168389dfe3b053897500c9d337b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/searchBookApp/static/js/runtime~main.336aca02.js"
   },
   {
-    "revision": "0a1aa6c924f513a7e4c5",
-    "url": "/searchBookApp/static/js/main.0a1aa6c9.chunk.js"
+    "revision": "7ccdae026814efde9e32",
+    "url": "/searchBookApp/static/js/main.7ccdae02.chunk.js"
   },
   {
     "revision": "fe0bf5abcd5464cefc6f",
     "url": "/searchBookApp/static/js/1.fe0bf5ab.chunk.js"
   },
   {
-    "revision": "0a1aa6c924f513a7e4c5",
+    "revision": "7ccdae026814efde9e32",
     "url": "/searchBookApp/static/css/main.4ff10a78.chunk.css"
   },
   {
-    "revision": "948cf7bdd32b5576d0f7c4c1145e84c6",
+    "revision": "18f80c6b425a58dca6f18b5ddf57e687",
     "url": "/searchBookApp/index.html"
   }
 ];
